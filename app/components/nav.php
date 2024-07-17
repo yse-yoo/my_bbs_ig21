0d9db14d6aa35b4916adf3fd051a846879cac1c7
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-07-17 10:14:08 +0900

    update

--- app/components/nav.php
+++ app/components/nav.php
@@ -1,12 +1,12 @@
 <nav class="bg-blue-500 p-4 flex items-center">
-    <a href="/">
+    <a href="./">
         <span class="text-2xl text-white hover:text-gray-200">My BBS</span>
     </a>
     <ul class="flex space-x-4 mx-3">
         <li>
-            <Link href="./thread/">
-            <span class="text-white hover:text-gray-200">スレッド</span>
-            </Link>
+            <a href="./thread/">
+                <span class="text-white hover:text-gray-200">スレッド</span>
+            </a>
         </li>
     </ul>
 </nav>
--- thread/index.php
+++ thread/index.php
@@ -14,6 +14,7 @@ $threads = $thread->get();
     <?php include(COMPONENTS_DIR . 'nav.php') ?>
 
     <div class="max-w-4xl mx-auto p-4">
+        <h1 class="text-3xl font-bold mb-4 text-center">スレッド一覧</h1>
         <div class="mb-6">
             <a href="./thread/input.php" class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
                 新しいスレッドを作成
@@ -21,7 +22,6 @@ $threads = $thread->get();
         </div>
 
         <div>
-            <h3 class="text-xl font-bold mb-4 text-center">スレッド一覧</h3>
             <?php if ($thread->values) : ?>
                 <?php foreach ($thread->values as $value) : ?>
                     <div class="border-b border-gray-300 mb-4 pb-2">
