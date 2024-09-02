af2256761036bede34df2ad29df3f0cd3745f9ac
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-09-02 09:49:44 +0900

    update

--- env.php
+++ env.php
@@ -6,6 +6,6 @@ const APP_KEY = "my_bbs";
 const DB_CONNECTION = 'mysql';
 const DB_HOST = '127.0.0.1';
 const DB_PORT = 3306;
-const DB_DATABASE = 'next_bbs';
+const DB_DATABASE = 'next-bbs';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
