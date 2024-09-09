706383117bca1c4d386ab4e248f83748b442bdcd
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-09-09 12:13:27 +0900

    env.sample 守勢

--- env.sample.php
+++ env.sample.php
@@ -7,5 +7,5 @@ const DB_CONNECTION = 'mysql';
 const DB_HOST = '127.0.0.1';
 const DB_PORT = 3306;
 const DB_DATABASE = 'next-bbs';
-const DB_USERNAME = 'yoo';
-const DB_PASSWORD = 'yoo';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
