7f678a3408fb07203379ccda9fc45f8e90dd89d5
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-09-09 12:09:36 +0900

    update

---
+++ env.sample.php
@@ -0,0 +1,11 @@
+<?php 
+const BASE_URL = "http://localhost/my_bbs_ig21/";
+const SITE_TITLE = "My BBS";
+const APP_KEY = "my_bbs";
+
+const DB_CONNECTION = 'mysql';
+const DB_HOST = '127.0.0.1';
+const DB_PORT = 3306;
+const DB_DATABASE = 'next-bbs';
+const DB_USERNAME = 'yoo';
+const DB_PASSWORD = 'yoo';
